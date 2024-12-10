bef3aa89344283907cfc8fe401230260c38f8280
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2024-12-10 19:11:56 +0100

    added jump

--- js/models/movable_object.class.js
+++ js/models/movable_object.class.js
@@ -4,18 +4,55 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 1.5;
+    otherDirection = false;
+    speedY= 0;
+    acceleration = 2.5;
+
+    applyGravity() {
+        setInterval(() => {
+            if(this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000/25);
+    }
+
+    isAboveGround() {
+        return this.y < 180;
+    }
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
-    moveRight() {
-        console.log('Moving right');
+    loadImages(arr) {
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
-    
+    moveRight() {
+        this.x += this.speed;
+    }
+
     moveLeft() {
-        
+        this.x -= this.speed;
+    }
+
+    jump() {
+        this.speedY = 30;
+    }
+
+    playAnimation(IMAGES_WALKING) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
     }
 }
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,35 +1,36 @@
 class World {
     character = new Character()
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObjects = [
-        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 80)
-    ]
-    canvas;
+    level = level1;
     ctx;
+    canvas;
+    keyboard;
+    camera_x = 0;
 
-    constructor(canvas) {
+    constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
+        this.keyboard = keyboard;
         this.draw();
+        this.setWorld();
     }
 
+    setWorld() {
+        this.character.world = this;
+    }
+
+    // Draw() wird immer wieder aufgerufen
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.backgroundObjects);
-        this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.enemies);
+        this.ctx.translate(this.camera_x, 0);
 
-        // Draw() wird immer wieder aufgerufen
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.level.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
+
         let self = this;
         requestAnimationFrame(function() {
             self.draw()
@@ -42,7 +43,22 @@ class World {
         });
     }
 
-    addToMap(mo){
-        this.ctx.drawImage(mo.img, mo.x, this.character.y, mo.width, mo.height);
+    addToMap(mo) {
+        if(mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
+        this.ctx.beginPath();
+        this.ctx.lineWidth = '5';
+        this.ctx.strokeStyle = 'blue';
+        this.ctx.rect(mo.x, mo.y, mo.x + mo.width, mo.y + mo.height);
+        this.ctx.stroke();
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        if(mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
